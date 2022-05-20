7ac6c841398ce4fb04a4bf6b572e36b3a0ca759e
Maxime Larquetoux <maximelarquetoux@gmail.com>
2022-05-20 10:56:20 +0200

    Mise en place du header

--- add-article.php
+++ add-article.php
@@ -8,8 +8,8 @@
 
 <head>
     <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="public/css/index.css">
-    <title>BLOG APP</title>
+    <link rel="stylesheet" href="public/css/add-article.css">
+    <title>Creer un article</title>
 </head>
 
 <body>
