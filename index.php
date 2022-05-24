82343041ddcce41ce71ff22dae437d89de812d4d
Maxime Larquetoux <maximelarquetoux@gmail.com>
2022-05-24 10:43:15 +0200

    Affichage des articles

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+$filename= __DIR__.'/data/articles.json';
+$articles=[];
+
+if(file_exists($filename)){
+    $articles= json_decode(file_get_contents($filename),true)??[];
+}
 
 
 ?>
@@ -16,7 +22,16 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
-
+        <div class="articles-container">
+        <?php foreach($articles as $article ):?>
+            <div class="article block">
+                <div class="overflow">
+                <div class="img-container" style="backgorund-image: url(<?=$article['image']?>);" ></div>
+                </div>
+                <h2 <?= $article['title'] ?>></h2>
+            </div>
+        <?php endforeach;?>
+        </div>
         </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
