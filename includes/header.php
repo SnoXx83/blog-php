fb6f6c4bc20e757572e57c084f166539a7293ae6
Maxime Larquetoux <maximelarquetoux@gmail.com>
2022-06-03 17:31:06 +0200

    filtrer les articles par categorie

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <header>
     <a class="logo" href="/">BLOG APP</a>
     <ul class="header-menu">
-        <li class="<?= $SERVER['REQUEST_URI'] === 'add-article.php' ? 'active' : '' ?>">
-            <a href="add-article.php">Ecrire un article</a>
+        <li class="<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>">
+            <a href="/add-article.php">Ecrire un article</a>
         </li>
     </ul>
 </header>
